90473ea13c20e710b7aecc2c49075452dcb61f4b
Jacob Krall <jacob@jkrall.net>
2014-08-03 16:24:20 -0400

    Lesson 2: OK02 - flash the ACT LED

--- source/main.s
+++ source/main.s
@@ -13,13 +13,28 @@ ldr r0,=0x20200000 @ base of GPIO
 mov r1,#1
 lsl r1,#18 @ 6th set of 3 bits = (6*3) = 18
 str r1,[r0,#4] @ 2nd set of 4 bytes = pins # 10-19 = [r0,#4]
-@ ^ prepare 16th GPI pin for output.
+@ ^ prepare 16th GPIO pin for output.
 
 mov r1,#1
 lsl r1,#16 @ pin 16!
-str r1,[r0,#40]
-@ ^ +40 is the address to turn a pin off (+28 turns it on)
+
 
 loop$:
-b loop$
 
+str r1,[r0,#40] @ +40 - pin off
+
+mov r2,#0x3F0000 @ about 2 seconds
+wait1$:
+sub r2,#1
+cmp r2,#0
+bne wait1$
+
+str r1,[r0,#28] @ +28 - pin on
+
+mov r2,#0x3F0000 @ about 2 seconds
+wait2$:
+sub r2,#1
+cmp r2,#0
+bne wait2$
+
+b loop$
